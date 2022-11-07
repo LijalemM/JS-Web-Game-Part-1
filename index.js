31039d5782b3788b2eb99c12a99558e6cb33d921
Your Name <you@example.com>
2022-11-06 21:35:17 -0800

    last update version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function newInmage (url, left, bottom) {
+function newImage (url, left, bottom) {
 
 let imageChar  = document.createElement('img')
 imageChar.src = url   
@@ -10,9 +10,25 @@ return (imageChar)
      
 }
 
-newInmage ('assets/green-character.gif', '100', '100')
-newInmage ('assets/pine-tree.png','450', '200')
-newInmage ('assets/tree.png', '200', '300')
-newInmage ('assets/pillar.png','350', '100')
-newInmage ('assets/crate.png', '150', '200')
-newInmage ('assets/well.png','500', '425')
+
+
+function newItem (url, left, bottom){
+    let newImageChar = newImage (url, left, bottom)
+    newImageChar.addEventListener ('dblclick' , function(){
+        console.log(newImageChar)
+        newImageChar.remove()
+        let inventoryItem = document.createComment('img')
+        inventoryItem.src = url      
+    })
+}
+newImage ('assets/green-character.gif', '100', '100')
+newImage ('assets/pine-tree.png','450', '200')
+newImage ('assets/tree.png', '200', '300')
+newImage ('assets/pillar.png','350', '100')
+newImage ('assets/crate.png', '150', '200')
+newImage ('assets/well.png','500', '425')
+
+newItem('assets/sword.png', '500', '530')
+newItem('assets/sheild.png', '165', '335')
+newItem('assets/staff.png', '600', '250')
+
