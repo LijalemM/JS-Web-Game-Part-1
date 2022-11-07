cc617cfb4a07ff6675ae976d83bdd54ccbc58c77
Your Name <you@example.com>
2022-11-06 19:16:58 -0800

    updated version

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newInmage (url, left, bottom) {
+
+let imageChar  = document.createElement('img')
+imageChar.src = url   
+imageChar.style.position = 'fixed'
+imageChar.style.left = left + "px" 
+imageChar.style.bottom = bottom + "px" 
+document.body.append(imageChar)
+return (imageChar)
+     
+}
+
+newInmage ('assets/green-character.gif', '100', '100')
+newInmage ('assets/pine-tree.png','450', '200')
+newInmage ('assets/tree.png', '200', '300')
+newInmage ('assets/pillar.png','350', '100')
+newInmage ('assets/crate.png', '150', '200')
+newInmage ('assets/well.png','500', '425')
